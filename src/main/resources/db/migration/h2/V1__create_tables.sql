7f210e4d3e8492af054f51c5c74369ca902e630f
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2018-10-18 09:23:41 +0200

    Change varchar to varchar2.

--- src/main/resources/db/migration/h2/V1__create_tables.sql
+++ src/main/resources/db/migration/h2/V1__create_tables.sql
@@ -4,10 +4,10 @@ create sequence txn_id_seq start with 10 increment by 50;
 
 create table users (
     id bigint default user_id_seq.nextval,
-    username varchar(255) not null,
-    password varchar(255) not null,
-    name varchar(255) not null,
-    email varchar(255) not null,
+    username varchar2(255) not null,
+    password varchar2(255) not null,
+    name varchar2(255) not null,
+    email varchar2(255) not null,
     enabled boolean not null,
     last_password_reset_date timestamp,
     primary key (id),
@@ -17,7 +17,7 @@ create table users (
 
 create table roles (
     id bigint default role_id_seq.nextval,
-    name varchar(255) not null,
+    name varchar2(255) not null,
     primary key (id),
     UNIQUE KEY role_name_unique (name)
 );
@@ -30,7 +30,7 @@ create table user_role (
 create table transactions (
     id bigint default txn_id_seq.nextval,
     amount decimal(12,2) not null,
-    type varchar(50) not null,
+    type varchar2(50) not null,
     created_on timestamp,
     user_id bigint REFERENCES users(id),
     primary key (id)
--- src/main/resources/db/migration/postgresql/V1__create_tables.sql
+++ src/main/resources/db/migration/postgresql/V1__create_tables.sql
@@ -4,10 +4,10 @@ create sequence txn_id_seq start with 10 increment by 50;
 
 create table users (
     id bigint DEFAULT nextval('user_id_seq') not null,
-    username varchar(255) not null CONSTRAINT user_username_unique UNIQUE,
-    password varchar(255) not null,
-    name varchar(255) not null,
-    email varchar(255) not null CONSTRAINT user_email_unique UNIQUE,
+    username varchar2(255) not null CONSTRAINT user_username_unique UNIQUE,
+    password varchar2(255) not null,
+    name varchar2(255) not null,
+    email varchar2(255) not null CONSTRAINT user_email_unique UNIQUE,
     enabled boolean not null,
     last_password_reset_date timestamp,
     primary key (id)
@@ -15,7 +15,7 @@ create table users (
 
 create table roles (
     id bigint DEFAULT nextval('role_id_seq') not null,
-    name varchar(255) not null CONSTRAINT role_name_unique UNIQUE,
+    name varchar2(255) not null CONSTRAINT role_name_unique UNIQUE,
     primary key (id)
 );
 
@@ -27,7 +27,7 @@ create table user_role (
 create table transactions (
     id bigint DEFAULT nextval('txn_id_seq') not null,
     amount decimal(12,2) not null,
-    type varchar(50) not null,
+    type varchar2(50) not null,
     created_on timestamp,
     user_id bigint REFERENCES users(id),
     primary key (id)
