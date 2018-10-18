ab2160bc64dc5dd9a6789742a9528f2a1eb1a7e9
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2018-10-18 07:59:02 +0200

    Using DTOs instead of entity.

--- src/main/java/com/sivalabs/expensetracker/web/controller/TransactionController.java
+++ src/main/java/com/sivalabs/expensetracker/web/controller/TransactionController.java
@@ -1,21 +1,29 @@
 package com.sivalabs.expensetracker.web.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 import com.sivalabs.expensetracker.config.CurrentUser;
 import com.sivalabs.expensetracker.entity.Transaction;
+import com.sivalabs.expensetracker.entity.User;
 import com.sivalabs.expensetracker.model.TransactionDTO;
 import com.sivalabs.expensetracker.security.SecurityUtils;
 import com.sivalabs.expensetracker.service.TransactionService;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import static org.springframework.http.HttpStatus.CREATED;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -23,63 +31,77 @@ import static org.springframework.http.HttpStatus.CREATED;
 @PreAuthorize("isAuthenticated()")
 public class TransactionController {
 
-    @Autowired
-    private CurrentUser currentUser;
+  @Autowired private CurrentUser currentUser;
 
-    private final TransactionService transactionService;
+  private final TransactionService transactionService;
 
-    @Autowired
-    public TransactionController(TransactionService transactionService) {
-        this.transactionService = transactionService;
-    }
+  @Autowired
+  public TransactionController(TransactionService transactionService) {
+    this.transactionService = transactionService;
+  }
 
-    @GetMapping("")
-    public List<TransactionDTO> getUserTransactions() {
-        log.info("current-user="+currentUser.getId());
-        log.info("process=get-user-transactions");
-        return transactionService.getUserTransactions(currentUser.getId())
-                .stream()
-                .map(this::convert).collect(Collectors.toList());
-    }
+  @GetMapping("")
+  public List<TransactionDTO> getUserTransactions() {
+    log.info("current-user=" + currentUser.getId());
+    log.info("process=get-user-transactions");
+    return transactionService
+        .getUserTransactions(currentUser.getId())
+        .stream()
+        .map(this::toDTO)
+        .collect(Collectors.toList());
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Transaction> getTransaction(@PathVariable("id") Long id) {
-        log.info("process=get-transaction, transaction_id={}", id);
-        Optional<Transaction> transaction = transactionService.getTransactionById(id);
-        return transaction.map( u -> ResponseEntity.ok(u))
-                   .orElse(ResponseEntity.notFound().build());
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity<TransactionDTO> getTransaction(@PathVariable("id") Long id) {
+    log.info("process=get-transaction, transaction_id={}", id);
+    Optional<Transaction> transaction = transactionService.getTransactionById(id);
+    return transaction
+        .map(u -> ResponseEntity.ok(toDTO(u)))
+        .orElse(ResponseEntity.notFound().build());
+  }
 
-    @PostMapping("")
-    @ResponseStatus(CREATED)
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
-        log.info("process=create-transaction");
-        transaction.setUser(SecurityUtils.getLoginUser().getUser());
-        return transactionService.createTransaction(transaction);
-    }
+  @PostMapping("")
+  @ResponseStatus(CREATED)
+  public TransactionDTO createTransaction(@RequestBody TransactionDTO transactionDTO) {
+    log.info("process=create-transaction");
+    Transaction transaction = toEntity(transactionDTO);
+    transaction.setUser(SecurityUtils.getLoginUser().getUser());
+    return toDTO(transactionService.createTransaction(transaction));
+  }
 
-    @PutMapping("/{id}")
-    public Transaction updateTransaction(@PathVariable("id") Long id, @RequestBody Transaction transaction) {
-        log.info("process=update-transaction, transaction_id={}", id);
-        transaction.setId(id);
-        transaction.setUser(SecurityUtils.getLoginUser().getUser());
-        return transactionService.updateTransaction(transaction);
-    }
+  @PutMapping("/{id}")
+  public TransactionDTO updateTransaction(
+      @PathVariable("id") Long id, @RequestBody TransactionDTO transactionDTO) {
+    log.info("process=update-transaction, transaction_id={}", id);
+    Transaction transaction = toEntity(transactionDTO);
+    transaction.setId(id);
+    transaction.setUser(SecurityUtils.getLoginUser().getUser());
+    return toDTO(transactionService.updateTransaction(transaction));
+  }
 
-    @DeleteMapping("/{id}")
-    public void deleteTransaction(@PathVariable("id") Long id) {
-        log.info("process=delete-transaction, transaction_id={}", id);
-        transactionService.deleteTransaction(id);
-    }
+  @DeleteMapping("/{id}")
+  public void deleteTransaction(@PathVariable("id") Long id) {
+    log.info("process=delete-transaction, transaction_id={}", id);
+    transactionService.deleteTransaction(id);
+  }
 
-    private TransactionDTO convert(Transaction txn) {
-       return TransactionDTO.builder()
-                .id(txn.getId())
-                .amount(txn.getAmount())
-                .type(txn.getType())
-                .createdOn(txn.getCreatedOn())
-                .userId(txn.getUser().getId())
-                .build();
-    }
+  private TransactionDTO toDTO(Transaction txn) {
+    return TransactionDTO.builder()
+        .id(txn.getId())
+        .amount(txn.getAmount())
+        .type(txn.getType())
+        .createdOn(txn.getCreatedOn())
+        .userId(txn.getUser().getId())
+        .build();
+  }
 
+  private Transaction toEntity(TransactionDTO dto) {
+    return Transaction.builder()
+        .id(dto.getId())
+        .amount(dto.getAmount())
+        .type(dto.getType())
+        .createdOn(dto.getCreatedOn())
+        .user(User.builder().id(dto.getUserId()).build())
+        .build();
+  }
 }
